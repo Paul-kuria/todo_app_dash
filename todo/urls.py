e0a7f13b3eed2bc9f43a028dbb9c98ec788d72c0
murani_k <paulmurani@gmail.com>
2023-10-10 18:20:33 +0300

    feat: todo app final code

--- todo/urls.py
+++ todo/urls.py
@@ -10,4 +10,6 @@ urlpatterns = [
 
     # Edit task
     path('edit_task/<int:pk>', views.edit_task, name='edit_task'),
+    # Delete task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task')
 ]
--- todo/views.py
+++ todo/views.py
@@ -25,4 +25,20 @@ def mark_as_undone(request, pk):
     return redirect('home')
 
 def edit_task(request, pk):
-    return render(request, 'edit_task.html')
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        # capture the new task the user is typing
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save() 
+        return redirect('home')
+    else:
+        context = {
+        'get_task':get_task,
+        }
+        return render(request, 'edit_task.html', context)
+    
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk) 
+    task.delete()
+    return redirect('home')
